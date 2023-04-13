6a7578ab29e7eead27cbfa771602819423326b67
ffekirnew <ffekirnew0808@gmail.com>
2023-04-13 11:54:13 +0300

    pushed the final changes

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,11 @@ hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("rotate-bar-2");
   hamburgerMenu.classList.toggle("rotate-bar-3");
 });
+
+$(".testimonial-slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+});
